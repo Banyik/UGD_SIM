4e79a8e38d8bcaaf221a6f1df712e142708a434f
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2024-08-02 13:56:00 +0200

    #0EX1 - Basic user info header

---
+++ UGD Simulator/UGD Simulator/UserInfo.h
@@ -0,0 +1,30 @@
+#pragma once
+
+struct Info{
+	unsigned short race;
+	unsigned short gender;
+	unsigned short identity;
+	unsigned short nationality;
+	unsigned short age;
+	unsigned short ideology;
+};
+
+struct Info* initialize_info;
+
+struct Personality {
+	//The Big Five Personalities
+};
+
+struct Personality* initialize_personality;
+
+struct FavoriteVideoCategories {
+
+};
+
+struct FavoriteVideoCategories* initialize_favorite_video_categories;
+
+struct FavoriteMusicCategories {
+
+};
+
+struct FavoriteMusicCategories* initialize_favorite_music_categories;
